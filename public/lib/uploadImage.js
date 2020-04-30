218ec56e9e4f74a9ec17a3ff0266513a697d397d
Wensi <wensi.peng@mail.mcgill.ca>
2020-04-30 18:59:36 -0400

    add upload image feature to all form pages

--- public/lib/uploadImage.js
+++ public/lib/uploadImage.js
@@ -5,10 +5,7 @@ reader.onload = function(event) {
 	var canvas = $("canvas");
 	var context = canvas.get(0).getContext("2d");
 	var imageURL = event.target.result;
-	//show the selected image
-	// $('.image-to-upload').attr("src", imageURL);
-	// $('.canvas').css("display", "block");
-	
+
 	var img = new Image();
 
 	//wait until the image has been fully loaded
@@ -17,20 +14,26 @@ reader.onload = function(event) {
         context.canvas.width  = img.width;
 		context.drawImage(img, 0, 0);
 		var cropper = new Cropper(canvas[0], {
-			aspectRatio: 6 / 19
+			aspectRatio: 9 / 16
 		})
-
+		//show crop and restore button
+		$(".crop-button, .restore-button").addClass("show-button");
+		//show canvas
+		$(".canvas-div").addClass("show-display");
 		//crop image
 		$(".crop-button").on("click", function(){
 			var croppedImageDataURL = cropper.getCroppedCanvas().toDataURL("image/*");
-			$(".image-canvas").append($('<img>').attr('src', croppedImageDataURL));
+			$(".image-display").empty();
+			$(".image-display").addClass("show-display");
+			$(".image-display").append($('<img>').attr('src', croppedImageDataURL));
 			//add the selected image to response
 			$('.image').attr("value", croppedImageDataURL);
 		})
 		//restore image
 		$(".restore-button").on("click", function(){
 			cropper.reset();
-			$(".image-canvas").empty();
+			$(".image-display").removeClass("show-display");
+			$(".image-display").empty();
 			//clear the selected image to response
 			$('.image').attr("value", "");
 		})
@@ -44,6 +47,13 @@ reader.error = function(event){
 }
 
 $(function(){
+	var oldImageURL = $('.image').attr("value");
+	//on edit page, show old image when the page is loaded
+	if(oldImageURL !== ""){
+			//show canvas
+			$(".image-display").addClass("show-display");
+			$(".image-display").append($('<img>').attr('src', oldImageURL));
+		}
 	$(".image-file").on("change", function(event){
 		// when #image-file is changed, new file is uploaded
 		var file = event.target.files[0];
